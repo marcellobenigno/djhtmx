12b143e6a3b04296eee23322cb126948b77745e3
Marcello Benigno <benigno.marcello@gmail.com>
2022-01-26 09:05:22 -0300

    erro no post da linha

--- djhtmx/expense/urls.py
+++ djhtmx/expense/urls.py
@@ -6,4 +6,6 @@ app_name = 'expense'
 urlpatterns = [
     path('', v.expense_list, name='expense_list'),
     path('adicionar/', v.expense_create, name='expense_create'),
+    path('<int:pk>/', v.expense_detail, name='expense_detail'),
+    path('<int:pk>/editar/', v.expense_update, name='expense_update'),
 ]
--- djhtmx/expense/views.py
+++ djhtmx/expense/views.py
@@ -10,9 +10,6 @@ def expense_list(request):
     object_list = Expense.objects.all()
     form = ExpenseForm()
 
-    if request.POST:
-        print('postou...')
-
     context = {
         'form': form,
         'object_list': object_list,
@@ -22,10 +19,42 @@ def expense_list(request):
 
 @require_http_methods(['POST'])
 def expense_create(request):
+    template_name = 'expense/hx/expense_hx.html'
     form = ExpenseForm(request.POST or None)
+    expense = None
 
     if form.is_valid():
         expense = form.save()
 
     context = {'object': expense}
-    return render(request, 'expense/hx/expense_hx.html', context)
+    return render(request, template_name, context)
+
+
+def expense_detail(request, pk):
+    template_name = 'expense/hx/expense_detail_hx.html'
+    obj = Expense.objects.get(pk=pk)
+    form = ExpenseForm(request.POST or None, instance=obj)
+    context = {
+        'object': obj,
+        'form': form,
+    }
+    return render(request, template_name, context)
+
+
+def expense_update(request, pk):
+    template_name = 'expense/hx/expense_hx.html'
+    obj = Expense.objects.get(pk=pk)
+    form = ExpenseForm(request.POST or None, instance=obj)
+
+    if request.method == 'POST':
+        print('post')
+        print(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'object': obj,
+    }
+
+    return render(request, template_name, context)
